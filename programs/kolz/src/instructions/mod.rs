c6159b42211727e93f8aedcce2e298c7ac05ada0
Colszoo <289228263+Colszoo@users.noreply.github.com>
2026-03-19 15:50:27 +0000

    refactor(program): re-export take_throne instruction

--- programs/kolz/src/instructions/mod.rs
+++ programs/kolz/src/instructions/mod.rs
@@ -7,7 +7,9 @@
 pub mod init_config;
 pub mod bind_launch;
 pub mod mint_kol_nft;
+pub mod take_throne;
 
 pub use init_config::*;
 pub use bind_launch::*;
 pub use mint_kol_nft::*;
+pub use take_throne::*;
